triton-inference-server/server

Update README and versions for 20.05 branch

Dockerfile
@@ -28,9 +28,9 @@
 # Multistage build.
 #
 
-ARG BASE_IMAGE=nvcr.io/nvidia/tensorrtserver:20.03-py3
-ARG PYTORCH_IMAGE=nvcr.io/nvidia/pytorch:20.03-py3
-ARG TENSORFLOW_IMAGE=nvcr.io/nvidia/tensorflow:20.03-tf1-py3
+ARG BASE_IMAGE=nvcr.io/nvidia/tensorrtserver:20.05-py3
+ARG PYTORCH_IMAGE=nvcr.io/nvidia/pytorch:20.05-py3
+ARG TENSORFLOW_IMAGE=nvcr.io/nvidia/tensorflow:20.05-tf1-py3
 
 ############################################################################
 ## PyTorch stage: Use PyTorch container for Caffe2 and libtorch

windyuan/triton-inference-server

Dockerfile
@@ -28,9 +28,9 @@
 # Multistage build.
 #
 
-ARG BASE_IMAGE=nvcr.io/nvidia/tritonserver:20.03-py3
-ARG PYTORCH_IMAGE=nvcr.io/nvidia/pytorch:20.03-py3
-ARG TENSORFLOW_IMAGE=nvcr.io/nvidia/tensorflow:20.03-tf1-py3
+ARG BASE_IMAGE=nvcr.io/nvidia/tritonserver:20.05-py3
+ARG PYTORCH_IMAGE=nvcr.io/nvidia/pytorch:20.05-py3
+ARG TENSORFLOW_IMAGE=nvcr.io/nvidia/tensorflow:20.05-tf1-py3
 
 ############################################################################
 ## PyTorch stage: Use PyTorch container for Caffe2 and libtorch